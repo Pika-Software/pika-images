61f5e55d5c28426da01e63f64b8eaed556d2ae88
Retro <44505837+dankmolot@users.noreply.github.com>
2021-05-27 10:27:11 +0300

    add java version output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /home/container
 echo "Docker image made by Retro#1593"
+java --version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
