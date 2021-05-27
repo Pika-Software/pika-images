a40077a52ea89364531f4dcb0a1225a3d63c0392
Retro <44505837+dankmolot@users.noreply.github.com>
2021-05-27 10:42:33 +0300

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home/container
 echo "Docker image made by Retro#1593"
-java --version
+java -version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
