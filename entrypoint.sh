02ed220406c3e47ceb5d2f05897b6ec521d708ed
Retro <44505837+dankmolot@users.noreply.github.com>
2021-05-29 12:42:28 +0300

    add libraries paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 cd /home/container
 echo "Docker image made by Retro#1593"
 
+# Add library paths
+export LD_LIBRARY_PATH=/home/container:/home/container/bin:/home/container/bin/linux32:/home/container/bin/linux64:/home/container/garrysmod/lua/bin
+
 # Run the Server
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 eval ${MODIFIED_STARTUP}
