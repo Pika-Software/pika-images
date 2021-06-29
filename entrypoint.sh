83785d93c318beb8e098b6af144e3cff74b1ba11
Retro <44505837+dankmolot@users.noreply.github.com>
2021-06-29 14:11:46 +0300

    added automating chmod for files and directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,17 @@ cd /home/container
 echo "Docker image made by Retro#1593"
 
 # Add library paths
-export LD_LIBRARY_PATH=/home/container:/home/container/bin:/home/container/bin/linux32:/home/container/bin/linux64:/home/container/garrysmod/lua/bin
+export LD_LIBRARY_PATH=./garrysmod/lua/bin
+
+# Set rights for files
+echo "Changing files rights..."
+find . -type d -exec chmod 770 {} \;
+find . -type f -exec chmod 660 {} \;
+find ./bin -type f -exec chmod 770 {} \;
+chmod -f 770 ./srcds_run
+chmod -f 770 ./srcds_run_x64
 
 # Run the Server
+echo "Starting server..."
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 eval ${MODIFIED_STARTUP}
