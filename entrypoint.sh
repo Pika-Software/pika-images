59a4f18b167db79833b3eb59ab04459f1b833047
Retro <44505837+dankmolot@users.noreply.github.com>
2021-06-29 14:16:47 +0300

    add some optimization

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ export LD_LIBRARY_PATH=./garrysmod/lua/bin
 echo "Changing files rights..."
 find . -type d -exec chmod 2770 {} \;
 find . -type f -exec chmod 660 {} \;
-find ./bin -type f -exec chmod 770 {} \;
+chmod -R 770 ./bin
 chmod -f 770 ./srcds_run
 chmod -f 770 ./srcds_run_x64
 
